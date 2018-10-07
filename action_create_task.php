9dde12b121e3bbe6bd61be2bd207fccae4f32cfd
Phung Tuan Hoang <phungtuanhoang1996@gmail.com>
2018-10-08 00:28:06 +0800

    commit

--- action_create_task.php
+++ action_create_task.php
@@ -4,21 +4,26 @@
     extract($_POST);
 
     // TODO: refactor this in future
-    $conn = pg_connect("host=" . DB_HOST . ", port=" . DB_PORT . ", dbname=" . DB_NAME);
+    $conn = pg_connect("host={$DB_HOST} port={$DB_PORT} dbname={$DB_NAME} user={$DB_USER} password={$DB_PASS}");
 
-    // TODO: what's the id here @Hoang
-    $id = '2';
-    // TODO: supposed to be DATETIME? Change SQL schematic?
-    $today = date("Y-m-d"); 
-
-    $query = "INSERT INTO tasks VALUES ('" . $id . "','" . $login_email . "', NULL,'" . $category . "','" . $today ."','" . $address . "','" . $description . "'," . $est_hours . ",0,0)";
-    $result = pg_query($conn, $query);
+    $uuid = uuid();
+    $result = pg_query($conn, "INSERT INTO tasks VALUES ({$uuid}, {$login_email}, NULL, {$category}, {$date}, {$address}, {$task_description}, {$estimated_hours}, 0, 0)");
 
     if ($result) {
         echo 'Successfully created task!';
         header("location: task_details.php?id=" . $id);
     } else {
         echo 'Some fields are not correct!';
-        header("location: create_task.php?error=1&category=".$category."&address=".$address."&description=".$description."&est_hour=".$est_hour);
+        header("location: create_task.php?error=1&category=".$category."&address=".$address."&description=".$task_description."&est_hour=".$estimated_hours);
+    }
+
+    function uuid() {
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0x0fff) | 0x4000,
+            mt_rand(0, 0x3fff) | 0x8000,
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
     }
 ?>
--- create_task.php
+++ create_task.php
@@ -8,12 +8,33 @@
         //TODO: extract GET variables if there are
     ?>
     <form action="action_create_task.php" method="POST">
-      Title: <input type="text" name="title"><br>
-      Description: <input type="text" name="description"><br>
-        <!-- TODO: dropdown for category -->
-      Category: <input type="text" name="category"><br>
-      Estimated time in hours: <input type="text" name="est_hours"><br>
-      <input type="submit">
+        <p>Task Category</p>
+        <select name="category">
+            <?php
+            include('config.php');
+            session_start();
+
+            if (!isset($_SESSION['login_user'])) {
+                header("Location: index.php");
+            } else {
+                $db = pg_connect("host={$DB_HOST} port={$DB_PORT} dbname={$DB_NAME} user={$DB_USER} password={$DB_PASS}");
+                $category = pg_query($db, "SELECT id, title FROM task_category");
+
+                while ($row = pg_fetch_assoc($category)) {
+                    echo "<option value=\"{$row['id']}\">{$row['title']}</option>";
+                }
+            }
+            ?>
+        </select>
+        <p>Date</p>
+        <input type="date" name="date"/>
+        <p>Address</p>
+        <input type="text" name="address"/><br/>
+        <p>Task Description</p>
+        <input type="text" name="task_description"/><br/>
+        <p>Estimated duration</p>
+        <input type="number" name="estimated_hours"/><br/>
+        <input type="submit"/>
     </form>
 </body>
 </html>
