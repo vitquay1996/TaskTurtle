61b4b2ab3c1e52d4c76e5ee1b38cc7df5e57a8cb
vitquay <vitquay1996@gmail.com>
2018-11-06 19:28:35 +0800

    find new tasks

--- action_get_my_own_tasks.php
+++ action_get_my_own_tasks.php
@@ -2,7 +2,7 @@
     require_once 'session.php';
 
     $conn = pg_connect("host={$DB_HOST} port={$DB_PORT} dbname={$DB_NAME} user={$DB_USER} password={$DB_PASS}");
-    $query = "SELECT t.id, t.description, t.date, t.time_start, t.time_end, count(*) as total_bidders, max(price) as max_bid from (tasks t inner join bids b on t.id = b.task_id) where t.requester_email = '{$_SESSION['login_user']}' group by t.id";
+    $query = "SELECT t.id, t.description, t.date, t.time_start, t.time_end, count(*) as total_bidders, min(price) as min_bid from (tasks t inner join bids b on t.id = b.task_id) where t.requester_email = '{$_SESSION['login_user']}' group by t.id";
 
     $result = pg_query($conn, $query);
     $resultArray = pg_fetch_all($result);
