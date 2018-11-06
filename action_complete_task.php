41fe6583d7ab6be7abf6d3e3b9287b24e6b110f2
vitquay <vitquay1996@gmail.com>
2018-11-06 22:17:33 +0800

    complete task action update action_get_my_own_tasks

---
+++ action_complete_task.php
@@ -0,0 +1,20 @@
+<?php
+    require_once 'config.php';
+    require_once 'session.php';
+    extract($_POST);
+
+    $conn = pg_connect("host={$DB_HOST} port={$DB_PORT} dbname={$DB_NAME} user={$DB_USER} password={$DB_PASS}");
+    $query = "UPDATE tasks SET is_finished = 1 WHERE id = '{$taskId}'";
+
+    $result = pg_query($conn, $query);
+    
+    if ($result) {
+        echo json_encode(array(
+            "success" => true
+        ));
+    } else {
+        echo json_encode(array(
+            "success" => false
+        ));
+    }
+?>
