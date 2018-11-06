95f27e623d04c4b2fa3a18abc7aa3fd02f8ce1aa
vitquay <vitquay1996@gmail.com>
2018-11-06 22:00:58 +0800

    minor parameters name change

--- action_get_my_own_tasks.php
+++ action_get_my_own_tasks.php
@@ -2,10 +2,13 @@
     require_once 'session.php';
     require_once 'config.php';
 
+    extract($_POST);
+
     $conn = pg_connect("host={$DB_HOST} port={$DB_PORT} dbname={$DB_NAME} user={$DB_USER} password={$DB_PASS}");
-    $query = "SELECT t.id, t.description, t.date, t.time_start, t.time_end, count(b.tasker_email) as total_bidders, min(price) as min_bid from (tasks t left outer join bids b on t.id = b.task_id) where t.requester_email = '{$_SESSION['login_user']}' group by t.id";
+    $query = "SELECT t.id, t.tasker_email, t.description, t.date, t.time_start, t.time_end, count(b.tasker_email) as total_bidders, min(price) as min_bid from (tasks t left outer join bids b on t.id = b.task_id) where t.requester_email = '{$_SESSION['login_user']}' group by t.id";
 
     $result = pg_query($conn, $query);
     $resultArray = pg_fetch_all($result);
     echo json_encode($resultArray);
+    
 ?>
