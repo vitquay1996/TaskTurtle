01990eda52bc9b85777e1cd1e14863bde030aabd
Quoc Cuong <nqcuong188@gmail.com>
2018-11-06 18:18:35 +0800

    commit Please enter the commit message for your changes. Lines starting

---
+++ action_get_categories.php
@@ -0,0 +1,13 @@
+<?php
+    require_once 'config.php';
+    require_once 'session.php';
+    extract($_POST);
+
+    $conn = pg_connect("host=" . DB_HOST . " port=" . DB_PORT . " dbname=" . DB_NAME . " user=" . DB_USER . " password=" . DB_PASS);
+    $query = "SELECT * FROM tasks_category";
+
+    $result = pg_query($conn, $query);
+    $resultArray = pg_fetch_all($result);
+    echo json_encode($resultArray);
+
+?>
