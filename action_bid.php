27727133d2b996b0730d6809d46af1d2e61d0cde
vitquay <vitquay1996@gmail.com>
2018-11-08 14:21:32 +0800

    add constraint safe bidding

--- action_bid.php
+++ action_bid.php
@@ -22,12 +22,23 @@
             ));
         }
     } else {
-        $query2 = "INSERT INTO bids VALUES ('{$_SESSION['login_user']}', '{$_POST['taskId']}',  '{$_POST['price']}', 0);";
+        // $query2 = "INSERT INTO bids VALUES ('{$_SESSION['login_user']}', '{$_POST['taskId']}',  '{$_POST['price']}', 0);";
+        $query2 = "SELECT safe_insert_bid('{$_SESSION['login_user']}', '{$_POST['taskId']}',  {$_POST['price']});";
+        // $query2 = "SELECT safe_insert_bid('zhanghan@gmail.com', '{$_POST['taskId']}',  {$_POST['price']});";
+
         $result2 = pg_query($conn, $query2);
+        $row = pg_fetch_row($result2);
         if ($result2) {
-            echo json_encode(array(
-                "success" => true
-            ));
+            if ($row[0] == 1) {
+                echo json_encode(array(
+                    "success" => true
+                ));
+            } else {
+                echo json_encode(array(
+                    "success" => false,
+                    "query" => $query2
+                ));
+            }
         } else {
             echo json_encode(array(
                 "success" => false,
--- action_get_completed_jobs.php
+++ action_get_completed_jobs.php
@@ -1,17 +1,17 @@
 <?php
     require_once 'config.php';
-    //require_once 'session.php'
+    require_once 'session.php';
     extract($_POST);
 
     $conn = pg_connect("host=" . DB_HOST . " port=" . DB_PORT . " dbname=" . DB_NAME . " user=" . DB_USER . " password=" . DB_PASS);
     $query = "SELECT t.id, t.description, t.date, t.time_start, t.time_end, u1.email, u1.full_name FROM tasks t, users u1, users u2 WHERE u1.email = t.requester_email and u2.email = '{$_SESSION['login_user']}' and t.is_finished = 1";
 
-    $result = pg_query($conn, $query)
+    $result = pg_query($conn, $query);
 
     if ($result){
         echo json_encode($result);
     } else {
-        echo json_encode(array())
+        echo json_encode(array());
     }
 
 ?>
